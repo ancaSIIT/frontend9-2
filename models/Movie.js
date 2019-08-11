e284fc5386f6bd668c44927e4132b2d801f98f22
cucgabrielpaul <cucgabrielpaul@yahoo.com>
2019-08-12 00:21:44 +0300

    Push Edit
    1/2 din edit

--- models/Movie.js
+++ models/Movie.js
@@ -1,14 +1,38 @@
 baseUrl = "https://movies-api-siit.herokuapp.com";
 
-  window.deleteMovieFromApi = function deleteMovieFromApi(movieId, authenticationToken) {
-    var url = baseUrl + "/movies/" + movieId;
-    
-    return fetch(url, {
-      method: "DELETE",
-      headers: {
-        "X-Auth-Token": authenticationToken
-      }
-    }).then(function(response) {
-      return response.text();
-    });
+window.deleteMovieFromApi = function deleteMovieFromApi(
+  movieId,
+  authenticationToken
+) {
+  var url = baseUrl + "/movies/" + movieId;
+
+  return fetch(url, {
+    method: "DELETE",
+    headers: {
+      "X-Auth-Token": authenticationToken
+    }
+  }).then(function(response) {
+    return response.text();
+  });
+};
+
+window.editMovieFromApi = function deleteMovieFromApi(movieId, titleValue) {
+  var data = {
+    Title: titleValue
+    Year: yearValue
+    Language: languageValue
+    Runtime: runtimeValue
+    Poster: posterValue
+    Genre: genreValue
+    imdbRating: imdbRatingValue    
   };
+  return fetch(this.baseUrl + "/movies/" + movieId, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "X-Auth-Token": authenticationToken
+    }
+  }).then(function(response) {
+    return response.json();
+  });
+};
