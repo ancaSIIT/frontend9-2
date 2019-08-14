385092a501c5c07f8153c03ad91053d650fb77e6
ioanavasarhelyi <moldovanyoana@yahoo.com>
2019-08-14 20:59:43 +0300

    verify logout

--- views/movieDetails.js
+++ views/movieDetails.js
@@ -3,24 +3,24 @@ var movieId = getQueryValue("id");
 console.log("Movie ID ", movieId);
 
 const movie = new Movie({ id: movieId });
-movie.get().then(data => {
-  console.log(data);
-  displayMovieHtml(data)
-})
-  .catch(error => {
-    document.body.innerText = "Invalid post ID";
+  movie.get().then(data => {
+    console.log(data);
+    displayMovieHtml(data)
+  })
+.catch(error => {
+  document.body.innerText = "Invalid post ID";
   });
 
 function getQueryValue(key) {
   var query = window.location.search.substring(1);
   var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] === key) {
-      return pair[1];
-    };
-  };
-  return (false);
+    for ( var i = 0; i < vars.length; i++ ) {
+      var pair = vars[i].split("=");
+          if( pair[0] === key ) {
+          return pair[1];
+           };
+     };
+   return(false);
 };
 
 const displayMovieHtml = data => {
@@ -28,27 +28,26 @@ const displayMovieHtml = data => {
   title.innerHTML = data.Title;
 
   let plot = document.querySelector(".plot");
-  plot.innerHTML = data.Plot;
+  plot.innerHTML= data.Plot;
 
   let year = document.querySelector(".year");
-  year.innerHTML = data.Year;
+  year.innerHTML=data.Year;
 
   let language = document.querySelector(".language");
-  language.innerHTML = data.Language;
+  language.innerHTML=data.Language;
 
   let runtime = document.querySelector(".runtime");
-  runtime.innerHTML = data.Runtime;
+  runtime.innerHTML=data.Runtime;
 
   let poster = document.querySelector(".posterUrl");
   poster.setAttribute("src", data.Poster);
 
   let genre = document.querySelector(".genre");
-  genre.innerHTML = data.Genre;
+  genre.innerHTML=data.Genre;
 
   let imdbRating = document.querySelector(".imdbRating");
-  imdbRating.innerHTML = data.imdbRating;
+  imdbRating.innerHTML=data.imdbRating;
 }
-
 
 const logoutDetailsBtn = document.querySelector(".logout-button");
 logoutDetailsBtn.addEventListener("click", logoutDetails);
@@ -57,6 +56,6 @@ function logoutDetails() {
    const logoutSession = new Auth();
    logoutSession.logout().then(data => {
      localStorage.removeItem("token");
-     verifyLoginDetails()
+     verifyLoginDetails();
    })
 }
