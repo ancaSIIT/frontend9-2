f8c9b723d7a30113ba53528b22216a1b56be1317
Andrei Turcas <tanenco@yahoo.com>
2019-08-19 19:51:33 +0300

    Login validations

--- views/home.js
+++ views/home.js
@@ -186,14 +186,48 @@ document.querySelector(".login-button").addEventListener("click", function () {
 document.querySelector("#btnLogin").addEventListener("click", function (e) {
   e.preventDefault();
   let username = document.getElementById("username").value;
+  let usernameElement = document.getElementById("username");
   let password = document.getElementById("password").value;
+  let passwordElement = document.getElementById("password");
+  let messageElement = document.querySelector(".loginValidateMessage");
   console.log(username, password);
-  let auth = new Auth();
-  auth.login(username, password).then(data => {
-    console.log(data);
-  })
 
-});
+  if (username == "") {
+    usernameElement.style.border = "2px solid red";
+    messageElement.innerHTML = "Please fill all fields."
+    messageElement.style.display = "inline";
+  } else {
+    usernameElement.style.border = "1px solid black";
+    messageElement.style.display = "none";
+  }
+  if (password == "") {
+    passwordElement.style.border = "2px solid red";
+    messageElement.innerHTML = "Please fill all fields."
+    messageElement.style.display = "inline";
+  } else {
+    passwordElement.style.border = "1px solid black";
+    messageElement.style.display = "none";
+  }
+  if (username && password) {
+    let auth = new Auth();
+  auth.login(username, password).then( data => {
+    console.log(data);
+    if (data.authenticated) {
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("user", username);
+      document.querySelector(".login-modal").style.display = "none";
+    //  verifyLoginHome();
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+      messageElement.style.display = "none";
+    } else {
+      messageElement.innerHTML = data.message;
+      messageElement.style.display = "inline";
+    }
+  })
+  }
+})
+
 
 document.querySelector(".login-close").addEventListener("click", function () {
   document.querySelector(".login-modal").style.display = "none";
@@ -204,19 +238,20 @@ document.querySelector(".message a").addEventListener("click", function () {
   document.querySelector(".login-modal").style.display = "none";
 })
 
-function validateLogin() {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  if (username == null || username == "") {
-    alert("Please enter the username.");
-    return false;
-  }
-  if (password == null || password == "") {
-    alert("Please enter the password.");
-    return false;
-  }
-  alert("Login successful.");
-}
+// function validateLogin() {
+//   let username = document.getElementById("username").value;
+//   let password = document.getElementById("password").value;
+//   if (username == null || username == "") {
+//     alert("Please enter the username.");
+//     return false;
+//   }
+//   if (password == null || password == "") {
+//     alert("Please enter the password.");
+//     return false;
+//   }
+//   document.querySelector(".login-modal").style.display = "none";
+//   document.querySelector(".user").innerHTML = username;
+// }
 
 //Register Button
 document.querySelector(".register-button").addEventListener("click", function () {
@@ -230,6 +265,3 @@ document.querySelector(".messageb a").addEventListener("click", function () {
   document.querySelector(".login-modal").style.display = "flex";
   document.querySelector(".reg-modal").style.display = "none";
 })
-
-
-
