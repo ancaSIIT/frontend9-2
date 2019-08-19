00b9e03934b9d952e6e9d3e5982fb43920c0f7d6
cucgabrielpaul <cucgabrielpaul@yahoo.com>
2019-08-19 15:59:56 +0300

    Edit Push
    Mai am de lucrat la partea de acces

--- models/Movie.js
+++ models/Movie.js
@@ -1,19 +1,31 @@
 var baseUrl = "https://movies-api-siit.herokuapp.com";
 
 class Movie {
-  constructor (options = {}) {
+  constructor(options = {}) {
     this.id = options.id;
   }
   get() {
-    return fetch(`${baseUrl}/movies/${this.id}`)
-     .then(response => {
-       if (response.ok) {
+    return fetch(`${baseUrl}/movies/${this.id}`).then(response => {
+      if (response.ok) {
         return response.json();
-        }
-        throw new Error("A network error occured", response.status);
-      });
-  };
+      }
+      throw new Error("A network error occured", response.status);
+    });
+  }
 }
+
+Movie.prototype.update = function(data) {
+  return fetch(`${baseUrl}/movies/${data.id}`, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json",
+      "x-Auth-Token": ""
+    }
+  }).then(function(response) {
+    return response.json();
+  });
+};
 
 function deleteMovieFromApi(movieId) {
   var url = baseUrl + "/movies/" + movieId;
@@ -21,10 +33,10 @@ function deleteMovieFromApi(movieId) {
   return fetch(url, {
     method: "DELETE",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       "x-Auth-Token": "9WnI_tKZbrC8fDB8FbL5gfFPcJsE8M9n"
     }
-  }).then(function (response) {
+  }).then(function(response) {
     return response.text();
   });
-};
+}
--- views/modalEdit.js
+++ views/modalEdit.js
@@ -4,39 +4,60 @@ let editClose = document.querySelector(".edit-close");
 let saveButton = document.querySelector(".save-button");
 let closeButton = document.querySelector(".close-button");
 
+let title = document.querySelector(".title");
+let plot = document.querySelector(".plot");
+let year = document.querySelector(".year");
+let language = document.querySelector(".language");
+let runtime = document.querySelector(".runtime");
+let poster = document.querySelector(".posterUrl");
+let genre = document.querySelector(".genre");
+let imdbRating = document.querySelector(".imdbRating");
+
+let inputTitle = document.getElementsByTagName("input")[1];
+let inputPlot = document.getElementsByTagName("input")[2];
+let inputYear = document.getElementsByTagName("input")[3];
+let inputLanguage = document.getElementsByTagName("input")[4];
+let inputRuntime = document.getElementsByTagName("input")[5];
+let inputPoster = document.getElementsByTagName("input")[6];
+let inputGenre = document.getElementsByTagName("input")[7];
+let inputImdbRating = document.getElementsByTagName("input")[8];
+
 editButton.onclick = () => {
-  let title = document.querySelector(".title").innerHTML;
-  let plot = document.querySelector(".plot").innerHTML;
-  let year = document.querySelector(".year").innerHTML;
-  let language = document.querySelector(".language").innerHTML;
-  let runtime = document.querySelector(".runtime").innerHTML;
-  let poster = document.querySelector(".posterUrl").innerHTML;
-  let genre = document.querySelector(".genre").innerHTML;
-  let imdbRating = document.querySelector(".imdbRating").innerHTML;
-
-  let inputTitle = document.getElementsByTagName("input")[1];
-  let inputPlot = document.getElementsByTagName("input")[2];
-  let inputYear = document.getElementsByTagName("input")[3];
-  let inputLanguage = document.getElementsByTagName("input")[4];
-  let inputRuntime = document.getElementsByTagName("input")[5];
-  let inputPoster = document.getElementsByTagName("input")[6];
-  let inputGenre = document.getElementsByTagName("input")[7];
-  let inputImdbRating = document.getElementsByTagName("input")[8];
-
-  inputTitle.setAttribute("value", title);
-  inputPlot.setAttribute("value", plot);
-  inputYear.setAttribute("value", year);
-  inputLanguage.setAttribute("value", language);
-  inputRuntime.setAttribute("value", runtime);
-  inputPoster.setAttribute("value", poster);
-  inputGenre.setAttribute("value", genre);
-  inputImdbRating.setAttribute("value", imdbRating);
+  inputTitle.setAttribute("value", title.innerHTML);
+  inputPlot.setAttribute("value", plot.innerHTML);
+  inputYear.setAttribute("value", year.innerHTML);
+  inputLanguage.setAttribute("value", language.innerHTML);
+  inputRuntime.setAttribute("value", runtime.innerHTML);
+  inputPoster.setAttribute("value", poster.src);
+  inputGenre.setAttribute("value", genre.innerHTML);
+  inputImdbRating.setAttribute("value", imdbRating.innerHTML);
 
   editModal.style.display = "block";
 };
+
 closeButton.onclick = () => {
   editModal.style.display = "none";
 };
+
 editClose.onclick = () => {
   editModal.style.display = "none";
+};
+
+var editData = new Movie({});
+
+saveButton.onclick = data => {
+  editData.Title = data.path[2].children[1].children[1].value;
+  editData.Year = data.path[2].children[1].children[3].value;
+  editData.Genre = data.path[2].children[1].children[5].value;
+  editData.Runtime = data.path[2].children[1].children[7].value;
+  editData.Plot = data.path[2].children[1].children[9].value;
+  editData.Language = data.path[2].children[1].children[11].value;
+  editData.Poster = data.path[2].children[1].children[13].value;
+  editData.imdbRating = data.path[2].children[1].children[15].value;
+  editData.id = movieId;
+
+  editData.update(editData).then(data => {
+    editModal.style.display = "none";
+    location.reload(true);
+  });
 };
